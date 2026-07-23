a374dc392a578b48a5b78a3772f726ed757aba10
Pierre C <pierre@chesneau.org>
2026-07-24 00:43:11 +0200

    Add WSL-specific configuration using NixOS Flakes

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # NixOs-Config
 My NixOs Configuration and note to quickly bootstrap my dev environnement
+
+## Enable flakes
+
+Add this to your `/etc/nix/configuration.nix`
+```nix
+    # /etc/nix/configuration.nix
+    # ....
+    
+    imports = [
+      # ...
+      ./system-customisation.nix
+    ];
+    
+    #...
+```
+
+Copy the `system-customisation.nix` file in this repository to your `/etc/nix/` directory.
+
+## Enable home-manager:
---
+++ wsl/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "WSL specific configuration for nixOS with Flakes";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl, ... }: {
+
+        module=   nixos-wsl.nixosModules.default
+          {
+            system.stateVersion = "26.05";
+            wsl.enable = true;
+            wsl.defaultUser = "nixos";
+};
+  };
+}
